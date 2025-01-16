7491956f05a4d01053672439a881d886bd05aa66
its-amann <amanagnihotri902@gmail.com>
2025-01-16 15:59:23 +0530

    Completed Data Validation Component

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 from networksecurity.components.data_ingestion import DataIngestion
+from networksecurity.components.data_validation import DataValidation
 from networksecurity.exception.exception import NetworkSecurityException
 from networksecurity.logging.logger import logging
-from networksecurity.entity.config_entity import DataIngestionConfig, TrainingPipelineConfig
+from networksecurity.entity.config_entity import DataIngestionConfig, TrainingPipelineConfig,DataValidationConfig
 
 if __name__ == "__main__":
     try:
@@ -13,6 +14,12 @@ if __name__ == "__main__":
         data_ingestion_artifact = data_ingestion.initiate_data_ingestion()  # No file_path needed
         print(data_ingestion_artifact)
         logging.info("Data Ingestion Completed")
+        logging.info("Initiating Data Validation")
+        data_validation_config = DataValidationConfig(training_pipeline_config)
+        data_validation = DataValidation(data_ingestion_artifact, data_validation_config)
+        data_validation_artifact = data_validation.initiate_data_validation()
+        logging.info("Data Validation Completed")
+        print(data_validation_artifact)
         
     except Exception as e:
         logging.error(f"An error occurred: {e}")
